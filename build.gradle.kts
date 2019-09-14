c3536cda27c7bbc3705bfe7567eaf934a1069b38
emilancius <erikas.m@pm.me>
2019-09-14 23:54:13 +0300

    Updated configuration to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.3.50"
+}
+
+group = "org.emgen.crypto.caesar"
+version = "1.0.0"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
